77ed5782df4837c96c3fe60b2d13445701a8a30d
ChrisGa <github@sugaree.net>
2018-07-14 09:37:32 -0700

    Initial Commit

---
+++ UITableViewDelegate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewDelegate
+//
+//  Created by Chris Gallagher on 7/14/18.
+//  Copyright © 2018 Chris Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
